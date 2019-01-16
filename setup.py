313796558abd5a78fa5b9e71220dcebf3512c095
tgermain <thomas_germain@trimble.com>
2019-01-16 13:27:21 +0100

    twine check

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='vr900-connector',
       version='0.0.1',
       description='Connector to handle vaillant vr900/vr920 data',
+      long_description='Connector to handle vaillant vr900/vr920 data',
       url='https://github.com/thomasgermain/vr900-connector.git',
       author='Thomas Germain',
       author_email='thomas.germain@live.be',
