d08c666ea249fa0e1791a9a7de9540cb0b4bd017
tgermain <thomas_germain@trimble.com>
2019-01-16 12:04:02 +0100

    configuration

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='vr900-connector',
-      version='0.1',
+      version='0.0.1',
       description='Connector to handle vaillant vr900/vr920 data',
       url='https://github.com/thomasgermain/vr900-connector.git',
       author='Thomas Germain',
       author_email='thomas.germain@live.be',
       license='MIT',
-      packages=['vr900-connector'],
-      zip_safe=False)
+      packages=['vr900connector'],
+      zip_safe=False,
+      install_requires=["responses", "requests"])
--- vr900connector/__init__.py
+++ vr900connector/__init__.py
@@ -2,4 +2,4 @@ from vr900connector.vr900connector import Vr900Connector
 from vr900connector.vr900connectorerror import Vr900ConnectorError
 import logging
 
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s:%(levelname)s:%(name)s: %(message)s")
