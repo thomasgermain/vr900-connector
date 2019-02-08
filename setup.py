5087b450e0a5d90f856fe5085995ff6713efd501
tgermain <thomas_germain@trimble.com>
2019-02-08 21:11:41 +0100

    excluding tests packages from the release

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='vr900-connector',
-      version='0.0.21',
+      version='0.0.23',
       description='Connector to handle vaillant vr900/vr920 data',
       long_description='Connector to handle vaillant vr900/vr920 data',
       url='https://github.com/thomasgermain/vr900-connector.git',
       author='Thomas Germain',
       author_email='thomas.germain@live.be',
       license='MIT',
-      packages=['vr900connector'],
+      packages=find_packages(exclude=('tests', 'tests/*', '/tests', '/tests/*')),
       zip_safe=False,
       setup_requires=["pytest-runner"],
       install_requires=["requests", "responses", "pytest"])
