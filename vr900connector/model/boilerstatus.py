83b1384df90e55d2aec3090b1e24e92140cfbd77
tgermain <thomas_germain@trimble.com>
2019-04-16 22:34:21 +0200

    adding error status, online status, update status

--- vr900connector/model/boilerstatus.py
+++ vr900connector/model/boilerstatus.py
@@ -14,10 +14,26 @@ class BoilerStatus:
         last_update: last update of the status (provided by the API)
     """
 
-    def __init__(self, device_name: str, description: str, title: str, code: str, hint: str, last_update: datetime):
+    def __init__(self, device_name: str, description: str, title: str, code: str, hint: str, last_update: datetime,
+                 online_status: str, update_status: str):
         self.device_name = device_name
         self.description = description
         self.title = title
         self.code = code
         self.hint = hint
         self.last_update = last_update
+        self.online_status = online_status
+        self.update_status = update_status
+
+    @property
+    def is_error(self) -> bool:
+        return self.code and (self.code.startswith('F') or self.code == 'con')
+
+    @property
+    def is_online(self):
+        return self.online_status == 'ONLINE'
+
+    @property
+    def is_up_to_date(self):
+        return self.update_status == 'UPDATE_NOT_PENDING'
+
